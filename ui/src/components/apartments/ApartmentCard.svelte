0aa67c3b0a4ec5aef2c5d8a92c2598cea22c5afe
MonkeyWhisper <82112471+MonkeyWhisper@users.noreply.github.com>
2023-08-11 16:08:52 -0400

    Fixed hard coded data for apartments

--- ui/src/components/apartments/ApartmentCard.svelte
+++ ui/src/components/apartments/ApartmentCard.svelte
@@ -1,27 +1,48 @@
 <script lang="ts">
     import type { IApartment } from '@typings/type'
-	import { fly } from 'svelte/transition'
+    import { fly } from 'svelte/transition'
+    import { PROPERTIES } from '@store/stores'
+    import { SendNUI } from '@utils/SendNUI'
+    import { onMount } from 'svelte'
 
-	export let selectedApartment: IApartment = null, apartment: IApartment = null, id="apt-card-0";
+    export let selectedApartment: IApartment = null;
+    export let apartment: IApartment = null;
+    export let id="apt-card-0";
 
-	// cbf with typescript rn
-	let apartmentData: any = apartment.apartmentData
+    let apartmentData: any;
+    let tenants: string[] = [];
+    let apartmentName: string;
+
+    $: apartmentData = apartment?.apartmentData || {};
+
+    $: if (apartment) {
+        apartmentName = apartment?.apartmentData.label as string;
+        const arrCitizenids = $PROPERTIES.filter((property) => property.apartment === apartmentName).map((property) => property.owner);
+
+        if (arrCitizenids.length !== 0 && arrCitizenids[0] !== undefined) {
+            SendNUI('getNames', arrCitizenids).then((names: string[]) => {
+                tenants = names;
+                apartmentData.currentTenants = tenants.length;
+            })
+        } else {
+            apartmentData.currentTenants = 0;
+        }
+    }
 </script>
 
 <div class="apartment-card-wrapper" id={id} 
     on:click={() => (selectedApartment = apartment)}
-	in:fly={{ y: 10, duration: 250 }} >
+    in:fly={{ y: 10, duration: 250 }} >
     
-    <img src={apartmentData.imgs[0].url} alt="Apartment Image" />
+    <img src={apartmentData.imgs?.[0]?.url} alt="Apartment Image" />
 
     <div class="details">
         <p class="heading">{apartmentData.label}</p>
-
         <p class="info">Legion Square</p>
 
         <div class="location-tenants-info">
-            <img src="images/user-location-pin.png" alt="User Location Pin Icon" />
-            <p>100 / 150 Global tenants</p>
+            <img src="images/user-location-pin.png" />
+            <p>{tenants.length || 0} Global Tenants</p>
         </div>
 
         <button class="select-apt-button">
