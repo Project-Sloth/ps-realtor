df67ce1e766d131f79877f52be44e0987cf9c15d
LeSiiN <103898231+LeSiiN@users.noreply.github.com>
2023-08-11 11:28:24 +0200

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -10,6 +10,46 @@ local blipsOwned = {}
 
 local UIOpen = false
 
+local tabletObj = nil
+local tabletDict = "amb@code_human_in_bus_passenger_idles@female@tablet@base"
+local tabletAnim = "base"
+local tabletProp = Config.AnimationProp
+local tabletBone = 60309
+local tabletOffset = vector3(0.03, 0.002, -0.0)
+local tabletRot = vector3(10.0, 160.0, 0.0)
+
+local function doAnimation()
+    if not UIOpen then return end
+    -- Animation
+    RequestAnimDict(tabletDict)
+    while not HasAnimDictLoaded(tabletDict) do Citizen.Wait(100) end
+    -- Model
+    RequestModel(tabletProp)
+    while not HasModelLoaded(tabletProp) do Citizen.Wait(100) end
+
+    local plyPed = PlayerPedId()
+    tabletObj = CreateObject(tabletProp, 0.0, 0.0, 0.0, true, true, false)
+    local tabletBoneIndex = GetPedBoneIndex(plyPed, tabletBone)
+
+    AttachEntityToEntity(tabletObj, plyPed, tabletBoneIndex, tabletOffset.x, tabletOffset.y, tabletOffset.z, tabletRot.x, tabletRot.y, tabletRot.z, true, false, false, false, 2, true)
+    SetModelAsNoLongerNeeded(tabletProp)
+
+    CreateThread(function()
+        while UIOpen do
+            Wait(0)
+            if not IsEntityPlayingAnim(plyPed, tabletDict, tabletAnim, 3) then
+                TaskPlayAnim(plyPed, tabletDict, tabletAnim, 3.0, 3.0, -1, 49, 0, 0, 0, 0)
+            end
+        end
+
+
+        ClearPedSecondaryTask(plyPed)
+        Citizen.Wait(250)
+        DetachEntity(tabletObj, true, false)
+        DeleteEntity(tabletObj)
+    end)
+end
+
 RegisterNetEvent('QBCore:Server:UpdateObject', function()
 	if source ~= '' then return false end
 	QBCore = exports['qb-core']:GetCoreObject()
@@ -22,6 +62,10 @@ local function toggleUI(bool)
 		action = "setVisible",
 		data = bool
 	})
+
+	if Config.PlayAnimation then
+		doAnimation()
+	end
 end
 
 RegisterNUICallback("hideUI", function()
@@ -29,7 +73,7 @@ RegisterNUICallback("hideUI", function()
 end)
 
 local function setRealtor(jobInfo)
-	if jobInfo.name == "realtor" then
+	if jobInfo.name == Config.RealtorJobName then
 		SendNUIMessage({
 			action = "setRealtorGrade",
 			data = jobInfo.grade.level
@@ -62,9 +106,21 @@ AddEventHandler("onResourceStart", function(resName)
 	end
 end)
 
-RegisterCommand("housing", function()
-	toggleUI(not UIOpen)
-end, false)
+if Config.UseCommand then
+	RegisterCommand("housing", function()
+		local PlayerData = QBCore.Functions.GetPlayerData()
+		if not PlayerData.metadata["isdead"] and not PlayerData.metadata["inlaststand"] and not PlayerData.metadata["ishandcuffed"] and not IsPauseMenuActive() then
+			toggleUI(not UIOpen)
+		end
+	end, false)
+end
+
+RegisterNetEvent('bl-realtor:client:toggleUI', function()
+	local PlayerData = QBCore.Functions.GetPlayerData()
+    if not PlayerData.metadata["isdead"] and not PlayerData.metadata["inlaststand"] and not PlayerData.metadata["ishandcuffed"] and not IsPauseMenuActive() then
+		toggleUI(not UIOpen)
+	end
+end)
 
 -- Callbacks
 RegisterNUICallback("setWaypoint", function (data, cb)
