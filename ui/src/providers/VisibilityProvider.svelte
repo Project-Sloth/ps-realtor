8bbed78efe842a674f7775b562c9cb8090206f8e
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2024-12-25 23:28:26 +0000

    remove unused backdropfix comp

--- ui/src/providers/VisibilityProvider.svelte
+++ ui/src/providers/VisibilityProvider.svelte
@@ -1,47 +1,43 @@
 <script lang="ts">
-  import { ReceiveNUI } from '../utils/ReceiveNUI';
-  import { SendNUI } from '../utils/SendNUI';
-  import { onMount } from 'svelte';
-  import { PROPERTIES, visibility } from '../store/stores';
-  import BackdropFix from './BackdropFix.svelte';
+	import { onMount } from 'svelte'
+	import { visibility } from '../store/stores'
+	import { ReceiveNUI } from '../utils/ReceiveNUI'
+	import { SendNUI } from '../utils/SendNUI'
 
+	let isVisible: boolean
 
-let isVisible: boolean;
+	visibility.subscribe((visible: boolean) => {
+		isVisible = visible
+	})
 
-visibility.subscribe((visible: boolean) => {
-  isVisible = visible;
-});
+	ReceiveNUI<boolean>('setVisible', (visible: boolean) => {
+		visibility.set(visible)
+	})
 
-ReceiveNUI<boolean>('setVisible', (visible: boolean) => {
-  visibility.set(visible);
-});
+	onMount(() => {
+		const keyHandler = (e: KeyboardEvent) => {
+			if (isVisible && ['Escape'].includes(e.code)) {
+				SendNUI('hideUI')
+				visibility.set(false)
+			}
+		}
 
-onMount(() => {
-  const keyHandler = (e: KeyboardEvent) => {
-    if (isVisible && ['Escape'].includes(e.code)) {
-      SendNUI('hideUI');
-      visibility.set(false);
-    }
-  };
+		window.addEventListener('keydown', keyHandler)
 
-  window.addEventListener('keydown', keyHandler);
-
-  return () => window.removeEventListener('keydown', keyHandler);
-});
+		return () => window.removeEventListener('keydown', keyHandler)
+	})
 </script>
 
 {#if isVisible}
-<main>
-    <slot />
-</main>
-<!-- <BackdropFix /> -->
+	<main>
+		<slot />
+	</main>
 {/if}
 
 <style>
-  main {
-    display: grid;
-    place-items: center;
-    height: 100vh;
-  }
+	main {
+		display: grid;
+		place-items: center;
+		height: 100vh;
+	}
 </style>
-
