7a44ca0f2572c6f40e1f8d720e6858884ce44302
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2024-12-27 22:05:23 +0000

    use card for list new property

--- ui/src/components/list-properties/ListPropertiesBase.svelte
+++ ui/src/components/list-properties/ListPropertiesBase.svelte
@@ -4,6 +4,7 @@
 	import { SendNUI } from '@utils/SendNUI'
 	import { SHELLS, TEMP_HIDE } from '@store/stores'
 	import Dropdown, { type LabelValue } from '@components/generic/Dropdown.svelte'
+	import Card from '@components/generic/Card.svelte'
 
 	const propertyTypes = [
 		{ label: 'MLO', value: 'mlo' }, 
@@ -98,189 +99,234 @@
 		</button>
 	</div>
 {:else}
-	<div class="list-new-property-form">
-		<div class="header">
-			<div class="heading-title-wrapper">
-				<i class="fas fa-circle-plus add-icon"></i>
-				<p>List New Property</p>
-			</div>
+	<Card title="List New Property">
+		<i class="fas fa-circle-plus" slot="icon"></i>
+		
+		<div class="left-column">
+			<p class="title">Property Information</p>
+
+			<p class="info">Make sure to fill everything out!</p>
 		</div>
 
-		<div class="body-wrapper overflow-y-auto">
-			<div class="left-column">
-				<p class="title">Property Information</p>
+		<div class="right-column">
+			<div id="property-type" class="form-row-wrapper">
+				<p class="label">Property Type</p>
 
-				<p class="info">Make sure to fill everything out!</p>
+				<div class="action-row">
+					<Dropdown 
+						items={propertyTypes}
+						bind:value={currentType}
+						prefix="Type:"
+					/>
+				</div>
 			</div>
-			<div class="right-column">
-				<div id="property-type" class="form-row-wrapper">
-					<p class="label">Property Type</p>
 
-					<div class="action-row">
-						<Dropdown 
-							items={propertyTypes}
-							bind:value={currentType}
-							prefix="Type:"
-						/>
-					</div>
+			{#if currentType === 'mlo'}
+			<div id="zone-creation" class="form-row-wrapper">
+				<p class="label">Zone Type</p>
+				<div class="action-row">
+					<SetNotSetIndicator
+						leftValue="Zone"
+						rightValue={zone_data ? 'Set' : 'Not Set'}
+						good={zone_data}
+					/>
+					<button
+						class="regular-button"
+						on:click={() => createZone('zone')}
+						>{zone_data ? 'Unset' : 'Set'}</button
+					>
 				</div>
+			</div>
+			{/if}
 
-                {#if currentType === 'mlo'}
-				<div id="zone-creation" class="form-row-wrapper">
-					<p class="label">Zone Type</p>
-					<div class="action-row">
-						<SetNotSetIndicator
-					    	leftValue="Zone"
-					    	rightValue={zone_data ? 'Set' : 'Not Set'}
-					    	good={zone_data}
-					    />
-						<button
-							class="regular-button"
-							on:click={() => createZone('zone')}
-							>{zone_data ? 'Unset' : 'Set'}</button
-						>
+
+			<div id="door-creation" class="form-row-wrapper door-creation-container">					
+				{#if currentType === 'mlo'}
+					<div class="label flex door-creation-title">
+						<span>Door Creation</span>
+						<div class="spacer"></div>
+						<button class="regular-button" on:click={addNewElement}><i class="fas fa-plus"></i></button>
 					</div>
-				</div>
-				{/if}
 
-
-				<div id="door-creation" class="form-row-wrapper door-creation-container">					
-					{#if currentType === 'mlo'}
-						<div class="label flex door-creation-title">
-							<span>Door Creation</span>
-							<div class="spacer"></div>
-							<button class="regular-button" on:click={addNewElement}><i class="fas fa-plus"></i></button>
-						</div>
-
-						{#each elements as element, index}
-							<div class="action-row" style="margin-bottom: 10px;">
-								<SetNotSetIndicator
-									leftValue="Door"
-									rightValue={element.door_data ? 'Set' : 'Not Set'}
-									good={element.door_data}
-								/>
-								<button
-									class="regular-button"
-									on:click={() => createZone('door', index)}
-									>{element.door_data ? 'Unset' : 'Set'}</button
-								>							
-
-								<button class="door-remove-btn" on:click={() => removeElement(index)}><i class="fas fa-close"></i></button>
-							</div>
-						{/each}
-					
-					{:else if currentType === 'shell'}
-						<p class="label">Door Creation</p>
-
+					{#each elements as element, index}
 						<div class="action-row" style="margin-bottom: 10px;">
 							<SetNotSetIndicator
 								leftValue="Door"
-								rightValue={elements[0].door_data ? 'Set' : 'Not Set'}
-								good={elements[0].door_data}
+								rightValue={element.door_data ? 'Set' : 'Not Set'}
+								good={element.door_data}
 							/>
 							<button
 								class="regular-button"
-								on:click={() => createZone('door', 0)}
-								>{elements[0].door_data ? 'Unset' : 'Set'}</button
-							>
+								on:click={() => createZone('door', index)}
+								>{element.door_data ? 'Unset' : 'Set'}</button
+							>							
+
+							<button class="door-remove-btn" on:click={() => removeElement(index)}><i class="fas fa-close"></i></button>
 						</div>
-					{/if}
+					{/each}
+				
+				{:else if currentType === 'shell'}
+					<p class="label">Door Creation</p>
 
-				</div>
-
-				<div id="garage-creation" class="form-row-wrapper">
-					<p class="label">Garage Creation</p>
-
-					<div class="action-row">
+					<div class="action-row" style="margin-bottom: 10px;">
 						<SetNotSetIndicator
-							leftValue="Garage"
-							rightValue={garage_data ? 'Set' : 'Not Set'}
-							good={garage_data}
+							leftValue="Door"
+							rightValue={elements[0].door_data ? 'Set' : 'Not Set'}
+							good={elements[0].door_data}
 						/>
 						<button
 							class="regular-button"
-							on:click={() =>
-								garage_data
-									? removeGarage()
-									: createZone('garage')}
-							>{garage_data
-								? 'Remove Garage'
-								: 'Set Garage'}</button
+							on:click={() => createZone('door', 0)}
+							>{elements[0].door_data ? 'Unset' : 'Set'}</button
 						>
-					</div>
-				</div>
-
-                {#if currentType !== 'mlo'}
-				<div id="garden-creation" class="form-row-wrapper">
-					<p class="label">Garden Creation</p>
-
-					<div class="action-row">
-						<SetNotSetIndicator
-							leftValue="Garden"
-							rightValue={garden_data ? 'Set' : 'Not Set'}
-							good={garden_data}
-						/>
-						<button
-							class="regular-button"
-							on:click={() =>
-								garden_data
-									? removeGarden()
-									: createZone('garden')}
-							>{garden_data
-								? 'Remove Garden'
-								: 'Set Garden'}</button
-						>
-					</div>
-				</div>
-				{/if}
-
-
-				<div id="description" class="form-row-wrapper">
-					<p class="label">Description</p>
-
-					<div class="action-row">
-						<textarea
-							rows="5"
-							placeholder="Write a short and sweet description about the property..."
-							bind:value={description}
-						/>
-					</div>
-				</div>
-
-				<div id="price" class="form-row-wrapper">
-					<p class="label">Price</p>
-
-					<div class="action-row">
-						<input
-							type="number"
-							placeholder="$1000000000"
-							bind:value={price}
-						/>
-					</div>
-				</div>
-
-                
-
-				{#if currentType === 'shell'}
-					<div id="shell-type" class="form-row-wrapper">
-						<p class="label">Shell Type</p>
-
-						<div class="action-row">
-							<Dropdown
-								items={shellTypes}
-								bind:value={shell}
-								prefix="Type: "
-							/>
-						</div>
 					</div>
 				{/if}
 
-                <div class="w-full h-[2vh]"></div>
 			</div>
+
+			<div id="garage-creation" class="form-row-wrapper">
+				<p class="label">Garage Creation</p>
+
+				<div class="action-row">
+					<SetNotSetIndicator
+						leftValue="Garage"
+						rightValue={garage_data ? 'Set' : 'Not Set'}
+						good={garage_data}
+					/>
+					<button
+						class="regular-button"
+						on:click={() =>
+							garage_data
+								? removeGarage()
+								: createZone('garage')}
+						>{garage_data
+							? 'Remove Garage'
+							: 'Set Garage'}</button
+					>
+				</div>
+			</div>
+
+			{#if currentType !== 'mlo'}
+			<div id="garden-creation" class="form-row-wrapper">
+				<p class="label">Garden Creation</p>
+
+				<div class="action-row">
+					<SetNotSetIndicator
+						leftValue="Garden"
+						rightValue={garden_data ? 'Set' : 'Not Set'}
+						good={garden_data}
+					/>
+					<button
+						class="regular-button"
+						on:click={() =>
+							garden_data
+								? removeGarden()
+								: createZone('garden')}
+						>{garden_data
+							? 'Remove Garden'
+							: 'Set Garden'}</button
+					>
+				</div>
+			</div>
+			{/if}
+
+
+			<div id="description" class="form-row-wrapper">
+				<p class="label">Description</p>
+
+				<div class="action-row">
+					<textarea
+						rows="5"
+						placeholder="Write a short and sweet description about the property..."
+						bind:value={description}
+					/>
+				</div>
+			</div>
+
+			<div id="price" class="form-row-wrapper">
+				<p class="label">Price</p>
+
+				<div class="action-row">
+					<input
+						type="number"
+						placeholder="$1000000000"
+						bind:value={price}
+					/>
+				</div>
+			</div>
+
+			
+
+			{#if currentType === 'shell'}
+				<div id="shell-type" class="form-row-wrapper">
+					<p class="label">Shell Type</p>
+
+					<div class="action-row">
+						<Dropdown
+							items={shellTypes}
+							bind:value={shell}
+							prefix="Type: "
+						/>
+					</div>
+				</div>
+			{/if}
+
+			<div class="w-full h-[2vh]"></div>
 		</div>
 
-		<div class="list-new-property-form-footer">
-			<button on:click={createPropertyMethod} disabled={!valid}>Create Property</button>
-		</div>
-	</div>
+		<button class="btn" on:click={createPropertyMethod} disabled={!valid} slot="footer">Create Property</button>		
+	</Card>
 {/if}
+
+<style>
+	:global(.card-header) > i {
+		color: var(--blue-color);
+	}
+
+	:global(.card-body) {
+		flex-direction: row;
+		gap: 2rem;
+	}
+
+	.left-column {
+		position: sticky;
+		top: 0;
+		display: flex;
+		flex-direction: column;
+		height: min-content;
+  	}
+
+	.left-column > .title {
+		font-weight: 600;
+	}
+
+	.left-column > .info {
+		font-weight: 400;
+		color: var(--white-color);
+		margin-top: 0.3vw;
+	}
+
+	.btn {
+		border-radius: 0.1vw;
+
+		width: fit-content;
+
+		font-weight: 500;
+
+		padding: 0.15vw 0.5vw;
+		text-align: center;
+		vertical-align: middle;
+
+		background: var(--light-border-color);
+		border: 1px solid var(--light-border-color-2);
+  	}
+
+	.btn:disabled {
+		color: rgba(255, 255, 255, 0.3);
+	}
+
+	.btn:hover:not(:disabled) {
+		color: rgba(255, 255, 255, 0.3);
+		border: 1px solid var(--light-border-color-6);
+	}
+</style>
