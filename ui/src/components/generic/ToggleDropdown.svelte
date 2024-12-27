e436372b28c932b88656154eb4b7af96fb3848f6
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2024-12-27 05:07:06 +0000

    add a simple dropdown to handle true/false setup

---
+++ ui/src/components/generic/ToggleDropdown.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import Dropdown, { type LabelValue } from './Dropdown.svelte'
+
+	const onValue = 'on';
+	const offValue = 'off';
+	
+    export let onLabel = 'on';
+    export let offLabel = 'off';
+	export let value: boolean;
+	export let prefix: string = '';
+	export let changed: (value: boolean, label: string) => void = () => null;
+
+	const items: LabelValue<string>[] = [
+		{ label: onLabel, value: onValue },
+		{ label: offLabel, value: offValue },
+	]
+
+	function select(innerValue: string) {
+		value = innerValue === onValue ? true : false;
+		changed(value, value ? onLabel : offLabel);
+	}
+</script>
+
+<Dropdown
+	items={items}
+	value={value ? onLabel : offLabel}
+	prefix={prefix}
+	changed={value => select(value)}
+/>
