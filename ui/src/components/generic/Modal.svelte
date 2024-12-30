8accde5e46a93a9f89258e44e99efdb3de42050e
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2024-12-30 03:28:12 +0000

    add modal component

---
+++ ui/src/components/generic/Modal.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    export let width: string | number = "60%";
+    export let height: string | number = "90%";
+</script>
+
+<article class="modal" style:width={width} style:height={height}>
+    <slot/>
+</article>
+
+<div class="modal-overlay"></div>
+
+<style>
+    .modal {
+        position: absolute;
+
+        z-index: 1;
+
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+    }
+
+    .modal-overlay {
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.6);
+    }
+</style>
