09cc512b5cb630fcc828a0648e81ae3ae267a505
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2025-01-01 04:19:16 +0000

    apartments refactor

--- ui/src/components/apartments/ApartmentsBase.svelte
+++ ui/src/components/apartments/ApartmentsBase.svelte
@@ -4,28 +4,38 @@
     import type { Apartment } from '@typings/type'
 	import SelectedApartment from "./SelectedApartment.svelte"
 
-    let selectedApartment: Apartment  = null;
-
-    $: {
-        if(selectedApartment) {
-            console.log("apartment selected: ", selectedApartment)
-        }
-    }
+    let selectedApartment: Apartment | null = null;
 </script>
-<div class="apartments-base">
-    <div class="heading">
-        All Apartments
-    </div>
 
+<h1>All Apartments</h1>
+
+<section class="apartment-listings">
     {#key $APARTMENTS}
-        <div class="apartment-listing-wrapper">
-            {#each $APARTMENTS as apartment, i}
-                <ApartmentCard bind:selectedApartment apartment={apartment} id={"apt-card-" + i} />
-            {/each}
-        </div>
+        {#each $APARTMENTS as apartment, i}
+            <ApartmentCard 
+                id={"apt-card-" + i}
+                {apartment}
+                selected={apartment => selectedApartment = apartment} 
+            />
+        {/each}
     {/key}
+</section>
 
-    {#if selectedApartment}
-        <SelectedApartment bind:selectedApartment />
-    {/if}
-</div>
+{#if selectedApartment}
+    <SelectedApartment bind:selectedApartment />
+{/if}
+
+<style>
+    .apartment-listings {
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(15rem, 1fr));
+		grid-auto-rows: 1fr;
+
+		gap: 1rem;
+		overflow-y: auto;
+
+		flex: 1;
+		padding: 0 1rem .5rem 1rem;
+    }
+</style>
+
