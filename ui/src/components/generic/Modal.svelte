8235c39e68c32db69ec071d341f6c6b2c5dae482
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2025-01-01 04:04:17 +0000

    give control of open/close state to modal

--- ui/src/components/generic/Modal.svelte
+++ ui/src/components/generic/Modal.svelte
@@ -1,19 +1,30 @@
 <script lang="ts">
     export let width: string | number = "60%";
     export let height: string | number = "90%";
+    export let open: boolean = false;
+    export let closed: () => void = () => null;
+
+    function close() {
+        open = false;
+        closed();
+    }
 </script>
 
-<article class="modal" style:width={width} style:height={height}>
-    <slot/>
-</article>
+<svelte:window on:keydown={(e) => e.key === 'Escape' && open && close()}></svelte:window>
 
-<div class="modal-overlay"></div>
+{#if open}
+    <article role="dialog" class="modal" style:width={width} style:height={height}>
+        <slot/>
+    </article>
+
+    <div class="modal-overlay" on:pointerdown={() => close()}></div>
+{/if}
 
 <style>
     .modal {
         position: absolute;
 
-        z-index: 1;
+        z-index: 5;
 
         left: 50%;
         top: 50%;
@@ -27,5 +38,6 @@
         width: 100%;
         height: 100%;
         background-color: rgba(0, 0, 0, 0.6);
+        z-index: 4;
     }
 </style>
